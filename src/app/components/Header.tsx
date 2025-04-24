e98f6e2faf0e82f3a31d62411f43178057b4593c
ZoirJohn <zoirbekzokirjonov2007@gmail.com>
2025-04-24 20:39:51 +0500

    added changing color while scrolling

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -2,16 +2,21 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 const Header = () => {
         const pathname = usePathname()
+        const [scroll, setScroll] = useState(false)
+        useEffect(() => {
+                window.addEventListener('scroll', () => setScroll(window.scrollY > 30))
+        }, [])
         return (
-                <header className='fixed w-full py-6 top-0'>
+                <header className={`fixed w-full py-6 top-0 ${scroll && 'bg-[#bee3fe]'} z-10`}>
                         <div className='wrapper flex items-center'>
                                 <Link href='/'>
                                         <Image src='/logo.png' alt='logo' width={125} height={36} priority />
                                 </Link>
-                                <nav className='mx-auto'>
+                                <nav className='mx-auto max-md:hidden'>
                                         <ul className='flex gap-x-10 max-lg:gap-x-5 max-lg:text-base'>
                                                 <li>
                                                         <Link href='/' className={`${pathname == '/' && 'text-highlight'}`}>
@@ -40,7 +45,7 @@ const Header = () => {
                                                 </li>
                                         </ul>
                                 </nav>
-                                <Link href='/Service' className='py-4 px-5 bg-highlight text-white font-semibold hover:opacity-90 transition-all'>
+                                <Link href='/Service' className='py-4 px-5 bg-highlight text-white font-semibold hover:opacity-90 transition-all max-md:ml-auto'>
                                         Consultation
                                 </Link>
                                 <span className='md:hidden relative w-8 ml-4'></span>
--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,5 +1,5 @@
 const NotFound = () => {
-        return <div className='pt-26'>404</div>
+        return <div className='pt-26 bg-[#bee3fe] min-h-screen'>404</div>
 }
 
 export default NotFound
